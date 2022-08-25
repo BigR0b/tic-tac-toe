b181f7d086216b7d01ccc639605f6d956a68ad43
Robin Rhodes <bigr0b@protonmail.com>
2022-08-24 23:25:10 -0400

    Create a javaScript function that will render the contents of the gameboard array to the webpage

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const gameBoard = (function () {
-  const _game = [];
+  const gameBoardArr = ['x', 'o', 'x', 'o', 'x', 'o'];
+  const gameBoardCells = document.querySelectorAll('.cell');
+  const addPiece = function () {
+    for (let i = 0; i < gameBoardArr.length; i++) {
+      gameBoardCells[i].textContent = gameBoardArr[i];
+    }
+  };
+  return { addPiece };
 })();
 
 const Player = (name) => {
   const playerName = name;
+  const wins = 0;
   return { playerName };
 };
 

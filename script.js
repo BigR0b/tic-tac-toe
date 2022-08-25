cc18ee6cbbbec66fd2bd756dfefb167bfdbe78db
Robin Rhodes <bigr0b@protonmail.com>
2022-08-24 22:54:33 -0400

    Add HTML board

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const gameBoard = (function () {
+  const _game = [];
+})();
+
+const Player = (name) => {
+  const playerName = name;
+  return { playerName };
+};
+
+const player1 = Player('Player 1');
+const player2 = Player('Player 2');

27c2f15ae8037b6fddbe8ad112146d01437e3211
Robin Rhodes <bigr0b@protonmail.com>
2022-08-25 22:59:25 -0400

    Add logic so the play on the gameboard is added to the array

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 'use strict';
 
 const gameBoard = (function () {
-  const gameBoardArr = ['x', 'o', 'x', 'o', 'x', 'o'];
-  const gameBoardCells = document.querySelectorAll('.cell');
+  let nextPlay = 0;
+  // prettier-ignore
+  const arr = [ , , ,
+                , , ,
+                , , ,];
+
+  const cells = document.querySelectorAll('.cell');
   const addPiece = function () {
-    for (let i = 0; i < gameBoardArr.length; i++) {
-      gameBoardCells[i].textContent = gameBoardArr[i];
+    for (let i = 0; i < arr.length; i++) {
+      cells[i].textContent = arr[i];
     }
   };
+
+  const playClick = function () {
+    if (!this.textContent) {
+      if (nextPlay === 0) {
+        this.textContent = 'X';
+        arr[this.dataset.cell] = this.textContent;
+        nextPlay++;
+      } else {
+        this.textContent = 'O';
+        arr[this.dataset.cell] = this.textContent;
+        nextPlay--;
+      }
+    }
+  };
+  const play = cells.forEach((cell) => {
+    cell.addEventListener('click', playClick);
+  });
+
   return { addPiece };
 })();
 
 const Player = (name) => {
   const playerName = name;
-  const wins = 0;
+
   return { playerName };
 };
 
